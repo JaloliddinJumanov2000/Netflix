0f6da8b5f4ea7a2fc91ce000b23fc9d2f12e810f
Jaloliddin <jumanovjaloliddin7@gmail.com>
2025-08-20 13:44:28 +0500

    new

--- movie/views.py
+++ movie/views.py
@@ -11,6 +11,9 @@ from movie.serializers import GenreSerializer, ContentSerializer
 def genre_list_or_create(request, format=None):
     if request.method == 'GET':
         genres = Genre.objects.all()
+        search = request.query_params.get('search', None)
+        if search:
+            genres = genres.filter(name__icontains=search)
         serializer = GenreSerializer(genres, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -39,9 +42,6 @@ def genre_retrive_update_delete(request, pk, format=None):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
     elif request.method == 'DELETE':
         genre.delete()
         return Response({"delete": "Object is delete!"}, status=status.HTTP_204_NO_CONTENT)
@@ -55,7 +55,14 @@ def content_retrive_update_or_delete(request, pk,format=None):
         return Response({"message": "Object not found"}, status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        serializer = ContentSerializer(content)
+        genre = request.query_params.get('genre')
+        director = request.query_params.get('director')
+        title = request.query_params.get('title')
+        desc = request.query_params.get('desc')
+        contents = Content.objects.all()
+        if genre:
+            contents = contents.filter(genres__name__icontains=genre)
+        serializer = ContentSerializer(contents)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     elif request.method == 'PUT':
